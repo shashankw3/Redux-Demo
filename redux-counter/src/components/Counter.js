e7f67bd1793985feabdbf2dd31275bad80dcd805
shashank <sskwankhede1@gmail.com>
2021-09-26 11:51:30 +0530

    counter ui improved

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -3,15 +3,32 @@ import React from 'react'
 import {connect} from 'react-redux'
 import { incrementCounter,decrementCounter} from '../redux/countAction'
 
+const button={
+    backgroundColor:'white',
+    border:'2px solid black',
+    padding:'20px',
+    margin:'10px'
+    
+}
+const heading={
+    display:'inline-block',
+    margin:'10px'
+}
+const divStyle={
+    border:'2px solid black',
+    display:'inline-block',
+    
+}
 
 
 function Counter(props) {
 
     return (
-        <div>
-            <h1>Count:{props.count}</h1>
-            <button onClick={props.increment}>+</button>
-            <button onClick={props.decrement}>-</button>
+        <div >
+            <h1>Redux-Counter</h1>
+            <button onClick={props.increment} style={button}>+</button>
+            <h1 style={heading}>{props.count}</h1>
+            <button onClick={props.decrement}  style={button}>-</button>
         </div>
     )
 }
